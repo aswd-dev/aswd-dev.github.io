1ca9b664c24aaff95d77f34e808f4b770526c3e5
aswd-developer <muhamedjakupi745@gmail.com>
2024-12-18 16:12:23 +0100

    first commit

---
+++ JAVA-SCRIPT/exercise/exercise3/script.js
@@ -0,0 +1,31 @@
+// alert("Hello JavaScript");
+
+let number = 1;
+let firstName = "Muhamed";
+let employed = true;
+let child;
+let nullValue = null;
+
+console.log(typeof number);
+console.log(typeof firstName);
+console.log(typeof employed);
+console.log(typeof child);
+console.log(typeof nullValue);
+
+/**************************************************/
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+console.log(sum);
+
+/**************************************************/
+
+let num = 5;
+let numString = "5";
+
+console.log(num > numString); // false
+console.log(num < numString); // false
+console.log(num == numString); // true
+console.log(num === numString); // false // edhe vleren e kontrolon edhe tipin (number ose string) nese sjan njejt eshte false
